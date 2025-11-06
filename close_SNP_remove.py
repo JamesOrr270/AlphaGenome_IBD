08d4d6875f53d95f71cb061672d97f3d57fd5c81
JamesOrr270 <jamesnorr@hotmail.co.uk>
2025-11-06 14:54:41 +0000

    update in structure

--- close_SNP_remove.py
+++ close_SNP_remove.py
@@ -1,24 +1,34 @@
 import pandas as pd
 
-close_SNPs = pd.read_csv('Results/close_SNPs/close_SNPs_extended.csv')
-IBD_SNPs = pd.read_csv('Data/IBD_SNPS.txt', header = None)[0].values.tolist()
-CD_SNPs = pd.read_csv('Data/CD_SNPS.txt',header = None)[0].values.tolist()
-UC_SNPs = pd.read_csv('Data/UC_SNPS.txt',header = None)[0].values.tolist()
+def close_SNPs_load(filepath):
+    close_SNPs = pd.read_csv(filepath)
+    close_SNPs_list = pd.concat([close_SNPs['SNP1'] , close_SNPs['SNP2']]).to_list()
+    return(close_SNPs_list)
 
-close_SNPs_list = pd.concat([close_SNPs['SNP1'] , close_SNPs['SNP2']]).to_list()
+def load_SNPs(filepath):
+    SNP_list = pd.read_csv(filepath, header = None)[0].values.tolist()
+    return(SNP_list)
 
-IBD_SNPs = [snp for snp in IBD_SNPs if not any(substring in str(snp) for substring in close_SNPs_list)]
-CD_SNPs = [snp for snp in CD_SNPs if not any(substring in str(snp) for substring in close_SNPs_list)]
-UC_SNPs = [snp for snp in UC_SNPs if not any(substring in str(snp) for substring in close_SNPs_list)]
+def remove_close_SNPs(disease_SNPs,close_SNP_list):
+    SNPs_removed = [snp for snp in disease_SNPs if not any(substring in str(snp) for substring in close_SNP_list)]
+    return(SNPs_removed)
 
-with open('Results/close_SNPs_Removed/IBD_SNPs_removed.txt', 'w') as f:
-    for item in IBD_SNPs:
-        f.write(f"{item}\n")
+def write_to_file(data,filepath):
+    with open(filepath, 'w') as f:
+        for item in data:
+            f.write(f"{item}\n")
 
-with open('Results/close_SNPs_Removed/CD_SNPs_removed.txt', 'w') as f:
-    for item in CD_SNPs:
-        f.write(f"{item}\n")
+def main():
 
-with open('Results/close_SNPs_Removed/UC_SNPs_removed.txt', 'w') as f:
-    for item in UC_SNPs:
-        f.write(f"{item}\n")
+    close_SNPs = close_SNPs_load('Results/close_SNPs/close_SNPs_extended.csv')
+
+    diseases = ['IBD', 'CD', 'UC']
+     
+    for disease in diseases:
+        
+        snps = load_SNPs(f'Data/{disease}_SNPS.txt')
+        filtered = remove_close_SNPs(snps, close_SNPs)
+        write_to_file(filtered, f'Results/close_SNPs_Removed/{disease}_SNPs_removed_test2.txt') 
+
+if __name__ == '__main__':
+    main()
