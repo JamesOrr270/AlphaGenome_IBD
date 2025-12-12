db85d9b412654d48fef6fc08a73f891c3a1a9653
Jamie Orr <jamesnorr@hotmail.co.uk>
2025-12-12 12:41:05 +0000

    Merged Datasets

--- Leuven_study/alternative_model_comparison.py
+++ Leuven_study/alternative_model_comparison.py
@@ -37,14 +37,61 @@ def generate_SNP_impact_lists():
     # Need to make it so that if the source gene has no name to use the orignial code as I think all the time it is just the miRNA
     df = pd.read_csv('/Users/jamesorr/Documents/Imperial/Project_1/AlphaGenome_IBD/Leuven_study/Data/genetic_data/affected_proteins_TFs_mirs_uc_gandalf_revision_gene_name_both.txt',
                      sep=',')
-    
-    FIMO_df = df[df['Interaction_source']=='FIMO']
-    RSAT_df = df[df['Interaction_source']=='RSAT']
-    MIRANDA_df = df[df['Interaction_source']=='MIRANDA']
+    models = ['FIMO','RSAT','MIRANDA']
 
-    FIMO_df = FIMO_df[['Source Gene','Target Gene', 'SNP']]
-    RSAT_df_df = RSAT_df_df[['Source Gene','Target Gene', 'SNP']]
-    MIRANDA_df_df = MIRANDA_df[['Source Gene','Target Gene', 'SNP']]
+    df['Source Gene'] = df['Source Gene'].fillna(df['Source'])
+
+    results = []  # Store all three results
+
+
+    for model in models:
+        model_df = df[df['Interaction_source'] == model]
+        model_df = model_df[['Target Gene', 'SNP']]
+        
+        grouped = model_df.groupby('SNP').agg({
+            'Target Gene': lambda x: list(set(x))
+        }).reset_index()
+
+        results.append(grouped)
+
+    return results[0], results[1], results[2]
+  
+def compare_with_AlphaGenome(FIMO_grouped_df,RSAT_grouped_df,MIRANDA_grouped_df):
+
+    # FIMO only predicts TF binding
+
+    dfs = {
+        '16KB': pd.read_csv('/Users/jamesorr/Documents/Imperial/Project_1/AlphaGenome_IBD/AlphaGenome/Results/AlphaGenome/All_Nonsig_SNPS_16KB_all_scores_RSID.csv'),
+        '100KB': pd.read_csv('/Users/jamesorr/Documents/Imperial/Project_1/AlphaGenome_IBD/AlphaGenome/Results/AlphaGenome/All_Nonsig_SNPS_100KB_all_scores_RSID.csv'),
+        '500KB': pd.read_csv('/Users/jamesorr/Documents/Imperial/Project_1/AlphaGenome_IBD/AlphaGenome/Results/AlphaGenome/All_Nonsig_SNPS_500KB_all_scores_RSID.csv'),
+        '1MB': pd.read_csv('/Users/jamesorr/Documents/Imperial/Project_1/AlphaGenome_IBD/AlphaGenome/Results/AlphaGenome/All_Nonsig_SNPS_1MB_all_scores_RSID.csv')
+    }
+
+    genes = {}
+    for window, df in dfs.items():
+        df = df[(df['quantile_score'] > 0.95) | (df['quantile_score'] < -0.95)]
+        df = df[['gene_name', 'RSID']]
+        df = df.groupby('RSID')['gene_name'].apply(lambda x: list(set(x))).reset_index()
+        df['RSID'] = df['RSID'].str.upper()
+        genes[window] = df
+
+    models = {
+        'FIMO': FIMO_grouped_df,
+        'RSAT': RSAT_grouped_df,
+        'MIRANDA': MIRANDA_grouped_df
+    }
+
+    merged_results = {}
+    for model_name, model_df in models.items():
+        for window, gene_df in genes.items():
+            merged = pd.merge(gene_df, model_df, left_on='RSID', right_on='SNP')
+            merged = merged[['RSID', 'gene_name', 'Target Gene']]
+            merged = merged.rename(columns={'gene_name': 'AlphaGenome', 'Target Gene': model_name})
+            merged_results[f'{model_name}_{window}'] = merged
+
+    print(merged_results['RSAT_16KB'])
+
 
 if __name__ == '__main__':
-    convert_source_data()
+    FIMO_grouped_df,RSAT_grouped_df,MIRANDA_grouped_df = generate_SNP_impact_lists()
+    gene_comparison_df = compare_with_AlphaGenome(FIMO_grouped_df,RSAT_grouped_df,MIRANDA_grouped_df)
